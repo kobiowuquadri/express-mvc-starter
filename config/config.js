277ac86f6f2183578f2965d24da41a0c65eb2456
kobiowuquadri <kobiowuq@gmail.com>
2024-05-28 08:52:16 -0500

    add: set up es6

---
+++ config/config.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+// MongoDB URL
+
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME || ''
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD || ''
+const MONGODB_URL = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@atlascluster.0d2q4pl.mongodb.net`
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 1337
+
+export const config = {
+    server: {
+        port : PORT
+    },
+    mongodb : {
+        url : MONGODB_URL
+    }
+}
