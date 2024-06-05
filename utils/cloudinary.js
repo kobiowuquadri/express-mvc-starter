24995e67dc40f75d9ed8e6aede840211adcb289d
kobiowuquadri <kobiowuq@gmail.com>
2024-06-05 12:05:03 -0500

    add: set up cloudinary for image upload

---
+++ utils/cloudinary.js
@@ -0,0 +1,9 @@
+import cloudinaryModule from 'cloudinary'
+
+export const cloudinary = cloudinaryModule.v2
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
