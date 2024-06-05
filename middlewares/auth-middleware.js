753a57be72b146043ef27a90b8995c8a91fef14f
kobiowuquadri <kobiowuq@gmail.com>
2024-06-05 02:23:02 -0500

    add: verify middleware

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,28 @@
+// Authorise User
+const jwt = require('jsonwebtoken')
+
+export const verify = (req, res, next) => {
+  
+    try {
+      const token = req.headers['authorization'].split(' ')[1]
+      // console.log(token)
+      if (!token) {
+        return res
+          .status(404)
+          .json({ success: false, message: 'Token not found!' })
+      }
+      jwt.verify(token, process.env.SECRET, async (err, decodedToken) => {
+        if (err) {
+          return res.status(401).json({ auth: false, message: 'Unauthorized' })
+        }
+        // console.log(decodedToken)
+        if (decodedToken) {
+          req.user = decodedToken.id
+          next()
+        }
+      })
+    } catch (err) {
+      console.log(err.message)
+    }
+  }  
+
