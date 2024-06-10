1709220c7b15ab2ddd4a4677a6eeb4fd7bfa8aad
kobiowuquadri <kobiowuq@gmail.com>
2024-06-10 04:14:19 -0500

    add: set up morgan(http logger middleware)

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ import { connectToDB } from './database/db'
 import { handleErrors } from './middlewares/errorHandler'
 import { authRoutes } from './routes/auth-routes'
 import helmet from 'helmet'
+import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express()
 const port = process.env.PORT
 
 // helmet to secure app by setting http response headers
 app.use(helmet());
+app.use(morgan('tiny'))
 
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+// cors config
+const corsOptions = {
+  origin: ['http://localhost:5000'],
+  optionsSuccessStatus: 200,
+}
 
+app.use(cors(corsOptions))
 
 // routes
 app.use('/api/v1/auth', authRoutes)
